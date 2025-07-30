5709b90ca2faa3d8facce3b5a9ab9cff5d6a121a
Symeonidis-Herzig, Alexandre (PG/R - Comp Sci & Elec Eng) <a.symeonidisherzig@surrey.ac.uk>
2025-07-30 15:46:01 +0100

    CI fixes

--- pokergym/__init__.py
+++ pokergym/__init__.py
@@ -3,5 +3,5 @@
 from pokergym.env.poker_env import PokerEnv
 from pokergym.env.config import PokerConfig
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __all__ = ["PokerEnv", "PokerConfig"]
--- setup.py
+++ setup.py
@@ -3,12 +3,21 @@ from setuptools import find_packages, setup
 
 setup(
     name="pokergym",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "gymnasium>=0.26.2",
-        "numpy>=1.21.0"],
+        "gymnasium>=1.2.0",
+        "pettingzoo>=1.25.0",
+        "numpy>=2.3.2",
+        "deuces>=0.2.1",
+        "tyro>=0.9",
+        "termcolor>=3.1",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+        ],
+    },
     author="Alexandre Symeonidis-Herzig",
     author_email="a.symeonidis.herzig@gmail.com",
-
 )
