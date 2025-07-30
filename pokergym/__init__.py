29cff43b50a18a36cb64eafd920251d995589faf
Symeonidis-Herzig, Alexandre (PG/R - Comp Sci & Elec Eng) <a.symeonidisherzig@surrey.ac.uk>
2025-07-30 15:40:02 +0100

    Adding CI tests with GitHub Actions

---
+++ pokergym/__init__.py
@@ -0,0 +1,7 @@
+"""PokerGym: A poker environment for reinforcement learning."""
+
+from pokergym.env.poker_env import PokerEnv
+from pokergym.env.config import PokerConfig
+
+__version__ = "0.0.1"
+__all__ = ["PokerEnv", "PokerConfig"]
